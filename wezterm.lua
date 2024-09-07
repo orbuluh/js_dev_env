4ab9e0c07575425479916521fb612bb376a98a37
orbuluh <scuendless@gmail.com>
2024-09-07 09:18:00 +0900

    update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -5,16 +5,11 @@ local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local act = wezterm.action
 
--- These are vars to put things in later
-local config = {}
+-- These are vars to put things in later (i dont use em all yet)
+local config = wezterm.config_builder()
 local keys = {}
 local mouse_bindings = {}
 local launch_menu = {}
-
--- This is for newer wezterm vertions to use the config builder
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
 
 -- Default config settings
 -- These are the default config settins needed to use Wezterm
@@ -23,52 +18,102 @@ end
 -- Color scheme, Wezterm has 100s of them you can see here:
 -- https://wezfurlong.org/wezterm/colorschemes/index.html
 config.color_scheme = 'Oceanic Next (Gogh)'
--- This is my chosen font, we will get into installing fonts on windows later
 config.font = wezterm.font('BlexMono Nerd Font')
 config.font_size = 11
 config.launch_menu = launch_menu
--- makes cursor blink
+-- makes my cursor blink
 config.default_cursor_style = 'BlinkingBar'
-config.disable_default_key_bindings = true
+
+config.disable_default_key_bindings = false
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = {
+    key = 'a',
+    mods = 'CTRL',
+    timeout_milliseconds = 1000
+}
+
 -- this adds the ability to use ctrl+v to paste the system clipboard
-config.keys = {{ key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },}
-config.mouse_bindings = mouse_bindings
+config.keys = {{
+    key = 'V',
+    mods = 'CTRL',
+    action = act.PasteFrom 'Clipboard'
+}, {
+    -- testing of LEADER:  ctrl+a --> r, sending woot!
+    key = 'R',
+    mods = 'LEADER',
+    action = act.SendString 'woot'
+}}
 
 -- There are mouse binding to mimc Windows Terminal and let you copy
--- To copy just highlight something and right click. Simple
-mouse_bindings = {
-  {
-    event = { Down = { streak = 3, button = 'Left' } },
-    action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
+-- To copy just highlight something and right click.
+mouse_bindings = { -- Change the default click behavior so that it only selects
+-- text and doesn't open hyperlinks
+{
+    event = {
+        Up = {
+            streak = 1,
+            button = "Left"
+        }
+    },
+    mods = "NONE",
+    action = act.CompleteSelection "PrimarySelection"
+}, -- select to copy(not wezterm copy mode), and paste if don't select anything
+{
+    event = {
+        Down = {
+            streak = 1,
+            button = 'Right'
+        }
+    },
     mods = 'NONE',
-  },
- {
-  event = { Down = { streak = 1, button = "Right" } },
-  mods = "NONE",
-  action = wezterm.action_callback(function(window, pane)
-   local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-   if has_selection then
-    window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-    window:perform_action(act.ClearSelection, pane)
-   else
-    window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-   end
-  end),
- },
-}
+    action = act.PasteFrom "Clipboard"
+}, -- CTRL-Click open hyperlinks
+{
+    event = {
+        Up = {
+            streak = 1,
+            button = "Left"
+        }
+    },
+    mods = "CTRL",
+    action = act.OpenLinkAtMouseCursor
+}, -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+-- https://wezfurlong.org/wezterm/config/mouse.html?highlight=Ctrl-click#gotcha-on-binding-an-up-event-only
+{
+    event = {
+        Down = {
+            streak = 1,
+            button = "Left"
+        }
+    },
+    mods = "CTRL",
+    action = act.Nop
+}, -- Grap the semantic zone when triple click
+{
+    event = {
+        Down = {
+            streak = 3,
+            button = "Left"
+        }
+    },
+    mode = "NONE",
+    action = act.SelectTextAtMouseCursor("SemanticZone")
+}}
+
+config.mouse_bindings = mouse_bindings
 
 -- This is used to make my foreground (text, etc) brighter than my background
 config.foreground_text_hsb = {
-  hue = 1.0,
-  saturation = 1.2,
-  brightness = 1.5,
+    hue = 1.0,
+    saturation = 1.2,
+    brightness = 1.5
 }
 
-
 -- IMPORTANT: Sets WSL2 UBUNTU-22.04 as the defualt when opening Wezterm
--- config.default_domain = 'WSL:Ubuntu'
+config.default_domain = 'WSL:Ubuntu'
 
 -- Or, specify with NixOS
-config.default_domain = 'WSL:NixOS'
+-- config.default_domain = 'WSL:NixOS'
 
 return config
